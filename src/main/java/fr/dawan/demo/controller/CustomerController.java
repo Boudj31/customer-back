ae5eecfdb78069932a57c5760a2e3e71412f7da7
gto <rachid.boudjenane@gmail.com>
2024-02-12 16:38:01 +0100

    fix : cross origin

--- src/main/java/fr/dawan/demo/controller/CustomerController.java
+++ src/main/java/fr/dawan/demo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class CustomerController {
 
     private final CustomerService customerService;
